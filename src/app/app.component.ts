f1257b4582a8997ac282c6823c9fa58653a5ae91
Nicholas Vaughn <vaughnick@gmail.com>
2020-05-27 18:52:05 -0400

    different turns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public translateExp = 'translateX(0px)';
+  public whosTurnIsIt = 'Red Team can start the game!';
+  public redTeamsTurn = true;
+  public redTeamsColor = 'rgba(224, 36, 36, 0.705)';
+  public blackTeamsColor = 'rgba(26, 26, 26, 0.705)';
 
-
-  hover(column) {
+  chipHoverTranslate(column) {
     this.translateExp = `translateX(${column}px)`;
+  }
+
+  toggleTurn() {
+    const blackTeamsTurn = 'It is Black Teams turn';
+    const redTeamsTurn = 'It is Red Teams turn';
+    // this.whosTurnIsIt = this.whosTurnIsIt === blackTeamsTurn ? redTeamsTurn : blackTeamsTurn;
+    if (this.whosTurnIsIt === blackTeamsTurn) {
+      this.whosTurnIsIt = redTeamsTurn;
+      this.redTeamsTurn = true;
+    } else {
+      this.redTeamsTurn = false;
+      this.whosTurnIsIt = blackTeamsTurn;
+    }
+
   }
 }
