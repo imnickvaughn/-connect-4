edbf8cedfc4d8ad103ff30da4fd863021d6f556b
Nicholas Vaughn <vaughnick@gmail.com>
2020-05-27 18:15:01 -0400

    chip moves over the game board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public translateExp = 'translateX(0px)';
+
+
+  hover(column) {
+    this.translateExp = `translateX(${column}px)`;
+  }
 }
